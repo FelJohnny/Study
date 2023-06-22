f8db844c580ee9229f673a109ff2580a3c520a0c
Felipe Johnny <felipijohnny@outlook.com>
2023-06-21 22:18:38 -0300

    iniciando JavaScript

---
+++ Front-End/_Origamid/HTML e CSS para Iniciantes/11 JavaScript Básico/02-manipular DOM/script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('nav');
+/*const cria variavel e a mesma nao pode ser declarada novamente*/
+/* document.querySelector()  puxa classes ou tags do html por exemplo*/
+const produtos = document.querySelector('.produtos a');
+console.log(nav);
+console.log(produtos);
+
+
+/*produtos.remove();*/
+
+console.dir(nav);
