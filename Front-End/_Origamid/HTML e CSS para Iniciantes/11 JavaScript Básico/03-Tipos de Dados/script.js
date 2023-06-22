57eef9a1ef7e7e34c76186a23da0ef1904f7af31
Felipe Johnny <felipijohnny@outlook.com>
2023-06-21 22:36:37 -0300

    tipos de dados JS

---
+++ Front-End/_Origamid/HTML e CSS para Iniciantes/11 JavaScript Básico/03-Tipos de Dados/script.js
@@ -0,0 +1,30 @@
+const texto = 'Curso de JS';
+const frase = 'meu nome "Johnny"';
+/*duas aspas */
+
+console.log(texto);
+
+let total = 0;
+const compras = 30;
+const imposto = 10;
+
+/*expressao matematica*/
+total = compras + imposto;
+
+const dobro = total * 2;
+
+console.log(total);
+
+console.log(dobro);
+
+/*concatenando strings */
+const string = "20" + "20";
+
+console.log(string)
+
+
+/*transformando string em numero */
+
+const transformar = Number("20") + 20;
+
+console.log(transformar)
