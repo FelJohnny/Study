1e6df949a945a8bc93fd8c4ff0ea2234ad67739b
Felipe Johnny <felipijohnny@outlook.com>
2023-06-21 23:36:05 -0300

    Entendendo Funcoes em js

---
+++ Front-End/_Origamid/HTML e CSS para Iniciantes/11 JavaScript Básico/Functions/script.js
@@ -0,0 +1,41 @@
+function NomeFunc(){
+    const nome = "JavaScript"
+    console.log(nome);
+}
+
+
+NomeFunc();
+NomeFunc();
+NomeFunc();
+
+
+/*o argumento(Johnny) que é oq retorna a function/parametro(nome)*/
+function Funcao2(nome){
+  console.log(nome);
+}
+
+Funcao2("Johnny");
+
+
+function Funcao3(nome, horas){
+    console.log(nome, horas, "horas");
+    return nome;
+}
+
+Funcao3("Maria", 40)
+
+
+const retornoDaFuncao = Funcao3("Ichigo", 40)
+
+console.log(retornoDaFuncao)
+
+/*retornando objeto */
+function Funcao4(nome, horas){
+    console.log(nome, horas, "horas");
+    return {
+        nome,
+        horas,
+    };
+}
+
+console.log(Funcao4);
