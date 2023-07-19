290021a6198ab031cb1b73157db84f20744f762f
Felipe Johnny <felipijohnny@outlook.com>
2023-07-19 20:28:40 -0300

    estudo sobre declaracao de variaveis

---
+++ Front-End/_Origamid/JavaScript/Variaveis/script.js
@@ -0,0 +1,32 @@
+var nome = 'Johnny'
+
+var idade = 23;
+
+var possuiFacul = true;
+console.log(nome, idade, possuiFacul);
+
+/*------------------------------ */
+
+
+var preco = 25;
+var totalcomprado = 5;
+var totalpreco = preco * totalcomprado;
+
+console.log(totalpreco);
+
+var sobrenome = 'Felipe',
+    cidade="suzano";
+
+    /*----------------------- */
+
+
+var semdefinir
+
+console.log("textoquarquer",semdefinir);
+
+
+/*---------------------EXERCICIO----------------------*/
+
+let Meunome = "Felipe Johnny", Minha_idade = "23", comida="batata frita";
+
+console.log(Meunome, Minha_idade, comida);
