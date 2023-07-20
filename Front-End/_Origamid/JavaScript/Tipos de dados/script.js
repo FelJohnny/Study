b3fae04a6be622f474e3eac167e549165eeb91d5
Felipe Johnny <felipijohnny@outlook.com>
2023-07-19 21:09:02 -0300

    exercitando tipos de variaveis

---
+++ Front-End/_Origamid/JavaScript/Tipos de dados/script.js
@@ -0,0 +1,40 @@
+let nome = "Johnny";
+
+/* TYPEOFF mostra o tipo da variavel */
+
+console.log(typeof nome);
+
+var sobrenome ='Felipe'
+
+var nomecompleto = nome + ' ' + sobrenome;
+
+console.log(nomecompleto);
+
+//template string
+
+let teste = `asdsad ${nome + nome}  dasdjk ${sobrenome + 1} asldhasdtesto` ;
+
+console.log(teste);
+
+/*Exercicio */
+
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+
+let empresa = "amalfis", cnpj = 682137682173, Minhaidade = 23, MeuNome = 'Felipe', MeuSobrenome = 'Johnny', MeuNomeCompleto= MeuNome + " " + MeuSobrenome;
+
+empresa =  empresa + " It's time";
+
+console.log(empresa);
+
+console.log("O tipo da variavel do meu nome completo e:",typeof MeuNomeCompleto);
