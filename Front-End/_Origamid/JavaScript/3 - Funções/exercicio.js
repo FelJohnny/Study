c248141ae2ac8d12a839a502646ab949d5d5da1d
Felipe Johnny <felipijohnny@outlook.com>
2023-07-21 10:14:11 -0300

    finalizando exercicio de funcoes do curso

--- Front-End/_Origamid/JavaScript/3 - Funções/exercicio.js
+++ Front-End/_Origamid/JavaScript/3 - Funções/exercicio.js
@@ -1,5 +1,12 @@
 // Crie uma função para verificar se um valor é Truthy
 
+function validaTruthy(dado){
+  return `o dado ${dado} inserido é ${!!dado}`;
+
+}
+
+console.log(validaTruthy(1));
+console.log(validaTruthy(0));
 
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
@@ -35,20 +42,42 @@ console.log(ParOuImpar(55))
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
 
+function TIpodado(tipo){
+  return `o tipo de dado(${tipo }) inserido é ${typeof(tipo)}`;
+}
+
+console.log(TIpodado('10'))
+
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'scroll' ocorrer.
+var nomefunc;
+
+  addEventListener('scroll', function(){
+    console.log(' contagem de scrolls Johnny')
+  });
+
+
+
+
+
 
 // Corrija o erro abaixo
-/*function precisoVisitar(paisesVisitados) {
+function precisoVisitar(paisesVisitados) {
   var totalPaises = 193;
-  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+  return `Foram visitado ${paisesVisitados} paises, ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+  
 }
+
+console.log(precisoVisitar(93));
+
 function jaVisitei(paisesVisitados) {
+  var totalPaises = 100;
   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
 }
-precisoVisitar(20);
-jaVisitei(20);
 
-*/
+console.log(jaVisitei(20));
+
+
+
