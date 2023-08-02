7fe2ab6bbc2da1d73e63813d89601f9475303514
Felipe Johnny <felipijohnny@outlook.com>
2023-08-02 20:10:22 -0300

    aplicando variaveis

---
+++ Front-End/DankiCode/PHP/modulo-05/3- variaveis/index.php
@@ -0,0 +1,16 @@
+<?PHP
+    $nome ='Johnny';
+    echo 'meu nome e<br/>';
+    echo $nome;
+
+    echo '<br/>meu nome e<br/>';
+    $nome = 'felipe johnny';
+    echo $nome;
+
+    //string
+    $nome = 'felipe johnny';
+    //inteiro
+    $idade = 23;
+
+    echo '<br/>',$idade;
+?>
