cdb224684ff9d56bc125ae693d898d1447868f94
Felipe Johnny <felipijohnny@outlook.com>
2023-08-01 21:07:43 -0300

    exercicios de manipulacao do DOM

---
+++ Front-End/_Origamid/JavaScript/7 - DOM/script.js
@@ -0,0 +1,13 @@
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+ativo = document.querySelector('.ativo');
+
+console.log(ativo);
+// Retorne a linguagem do navegador
+console.log(window.navigator.language);
+// Retorne a largura da janela 
+
+console.log(window.innerWidth);
