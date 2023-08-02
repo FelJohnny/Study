3bc17ef1b70e57a65eb3186063fe9ad88fe35a99
Felipe Johnny <felipijohnny@outlook.com>
2023-08-02 20:00:00 -0300

    praticando aula 2

---
+++ Front-End/DankiCode/PHP/modulo-05/1-aula/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de teste</title>
+</head>
+<body>
+    <p>
+        <?php 
+            echo 'ola mundo';
+        
+        ?>
+    </p>
+</body>
+</html>
---
+++ Front-End/DankiCode/PHP/modulo-05/2-aula/index.php
@@ -0,0 +1,13 @@
+<?PHP
+    echo '<pre>';
+    echo $_SERVER['SERVER_NAME']; // mostra o nome do servidor 
+    echo '<pre>';
+    echo $_SERVER['DOCUMENT_ROOT'];//mostra o caminho do doc
+    
+    echo '<pre>';
+        print_r($_SERVER); //
+    echo '<pre>';
+    
+        
+
+?>
