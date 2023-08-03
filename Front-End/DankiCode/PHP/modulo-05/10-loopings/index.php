bc9199dd561e78c71dac6fe7de2844621db03933
Felipe Johnny <felipijohnny@outlook.com>
2023-08-02 21:50:53 -0300

    estudando estrutura de repeticao em PHP

---
+++ Front-End/DankiCode/PHP/modulo-05/10-loopings/index.php
@@ -0,0 +1,24 @@
+<?PHP
+    for($i = 1; $i <= 10; $i++){
+        echo 'ola mundo';
+        echo $i;
+        echo '<hr>';
+    }
+
+    $i = 0;
+    while($i <= 10){
+        echo 'ola mundo WHILE';
+        echo $i;
+        echo '<hr>';
+        $i++;
+    }
+
+
+$i= 1;    
+do{
+    echo 'ola mundo DO WHILE';
+        echo $i;
+        echo '<hr>';
+        $i++;
+}while($i<10);
+?>
