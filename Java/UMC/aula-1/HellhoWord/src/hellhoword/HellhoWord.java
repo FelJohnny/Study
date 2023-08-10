cbbe917ed27627e698e59bb15048857391ee038a
unknown <alunos@018LAB1231.umc.br>
2023-08-10 20:50:55 -0300

    Hello word em Java

--- Java/UMC/aula-1/HellhoWord/src/hellhoword/HellhoWord.java
+++ Java/UMC/aula-1/HellhoWord/src/hellhoword/HellhoWord.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package hellhoword;
 
-/**
- *
- * @author alunos
- */
 public class HellhoWord {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ola mundo"); //ola mundo
     }
     
 }
