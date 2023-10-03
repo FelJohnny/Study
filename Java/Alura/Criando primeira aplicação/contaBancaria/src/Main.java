541bbd35e219f8553ead9af24eecf21fe1ab9428
Felipe Johnny <felipijohnny@outlook.com>
2023-10-03 13:21:18 -0300

    iniciando projeto do curso

---
+++ Java/Alura/Criando primeira aplicação/contaBancaria/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Informe o seu nome: ");
+        String nome = leitura.nextLine();
+        System.out.println(nome);
+        }
+    }
