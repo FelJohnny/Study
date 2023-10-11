35d20bf119385ef379ffd0c9998531bebb0eb922
Felipe Johnny <felipijohnny@outlook.com>
2023-10-11 10:42:49 -0300

    aplicando foreach em listas

--- Front-End e Back-End/Origamid/JavaScript/5 - array-e-loops/script.js
+++ Front-End e Back-End/Origamid/JavaScript/5 - array-e-loops/script.js
@@ -16,3 +16,35 @@ console.log(videoGames); // Adiciona ao final da array
 
 
 videoGames.length; // quantidade de itens no array 3
+
+//FOR LOOP
+
+for (var numero = 0; numero < 10; numero++) {
+    console.log(numero);
+
+}
+// Retorna de 0 a 9 no console
+  
+
+//WHILE LOOP
+
+var i = 0;
+while (i < 10) {
+console.log(i);
+i++;
+}
+// Retorna de 0 a 9 no console
+
+
+
+for(let i = 0; i < videoGames.length; i++){
+    console.log("array "+i +" "+ videoGames[i]);
+}
+
+
+//ou
+
+
+videoGames.forEach((item) => {
+    console.log("foreach "+item);
+});
