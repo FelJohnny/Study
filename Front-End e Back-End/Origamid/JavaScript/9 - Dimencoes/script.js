afa2547b91bab3485ee6102f3a06b7059754c665
Felipe Johnny <felipijohnny@outlook.com>
2023-10-12 12:09:37 -0300

    aplicando praticas de dimensões em Js

--- Front-End e Back-End/Origamid/JavaScript/9 - Dimencoes/script.js
+++ Front-End e Back-End/Origamid/JavaScript/9 - Dimencoes/script.js
@@ -1,92 +1,26 @@
- const menu = document.querySelector('.menu');
-
-// menu.className; // string
-// menu.classList; // lista de classes
-// menu.classList.add('ativo');
-// menu.classList.add('ativo', 'mobile'); // duas classes
-// menu.classList.remove('ativo');
-// menu.classList.toggle('ativo'); // adiciona/remove a classe
-// menu.classList.contains('ativo'); // true ou false
-// menu.classList.replace('ativo', 'inativo');
-
-menu.classList.add('azul');
-console.log(menu.classList);
-
-if(menu.classList.contains('azul')){
-    console.log("possui classe azul")
-}else{
-    console.log("não possui")
-}
-
-const animais = document.querySelector('.animais');
-
-//mostra os atribtos da tag
-console.log(animais.attributes)
-//seleciona a posição do atrubuto
-console.log(animais.attributes[1])
-
-const img = document.querySelector('img')
-
-//puxa o conteudo do atributo
-console.log(img.getAttribute('src'));
-//define um novo atributo ou substitui existente
-img.setAttribute('alt', 'é uma raposa');
-
-console.log(img.getAttribute('alt'));
-//remove atributo
-img.removeAttribute('alt')
-console.log(img.getAttribute('alt'));
+console.log("Verifique a distância da primeira imagem em relação ao topo da página");
+const imagem = document.querySelector("img");
+// propriedade offsetTop , left, ri informa a distancia do elemento até o final da pagina direcionada (esquerda, direita etc)
+console.log(`R: a primeira imagem possui ${imagem.offsetTop} pixels de distancia até o topo da pagina`);
 
 
 
+console.log( "Retorne a soma da largura de todas as imagens");
 
-// Adicione a classe ativo a todos os itens do menu
-console.log("Adicione a classe ativo a todos os itens do menu")
+const listImg = document.querySelectorAll("img");
 
-const menuex = document.querySelectorAll(".menu li");
+let soma = 0;
 
-menuex.forEach((item) => {
-    console.log(item)
-    
-    item.classList.add('ativo');
+//offsetWidth Top
+listImg.forEach((imgArray)=> {
+    soma += imgArray.offsetWidth;
 });
 
-// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
-console.log('Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro');
+console.log("R: a soma das larguras de todas imagens é: "+soma);
 
-menuex.forEach((item) => {
-    console.log(item)
-    item.classList.remove('ativo');
-});
-menuex[0].classList.add('ativo');
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
 
-
-// Verifique se as imagens possuem o atributo alt
-
-console.log("Verifique se as imagens possuem o atributo alt")
-
-const imagens = document.querySelectorAll('img');
-console.log(imagens)
-//teste condicional
-imagens[0].setAttribute('alt','teste');
-imagens[4].setAttribute('alt','teste');
-
-imagens.forEach((img, index) =>{
-    if(img.hasAttribute('alt')){
-        console.log("imagem "+ index +" possui o alt como atributo");
-    }else{
-        console.log("imagem "+ index +" NÃO possui o alt como atributo");
-    }
-}) 
-// Modifique o href do link externo no menu
-
-console.log("Modifique o href do link externo no menu");
-
-const link = document.querySelector('a[href^="http"');
-
-console.log("antes")
-console.log(link)
-console.log("depois")
-link.setAttribute('href','google.com')
-console.log(link)
-
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
