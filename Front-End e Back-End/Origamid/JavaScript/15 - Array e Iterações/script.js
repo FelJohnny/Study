e4ac4e9f67ad798a0a81e08f7f368d80fb731b8c
Felipe Johnny <felipijohnny@outlook.com>
2023-10-23 07:54:57 -0300

    aplicando conceitos de array

--- Front-End e Back-End/Origamid/JavaScript/15 - Array e Iterações/script.js
+++ Front-End e Back-End/Origamid/JavaScript/15 - Array e Iterações/script.js
@@ -44,19 +44,65 @@ const aulas = [
 ]
 
 
-
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
 
 const cursos = document.querySelectorAll('.curso');
 
 const arrayCursos = Array.from(cursos);
 
-const objetoCursos =arrayCursos.map((curso)=>{
-  console.log(curso);
-  const titulo = curso.document.querySelector('h1');
-  const descricao = curso.document.querySelector('p');
-  const aulas = curso.document.querySelector('.aulas');
-  const horas = curso.document.querySelector('h1');
+const objetoCursos = arrayCursos.map((curso)=>{
+  const titulo = curso.querySelector('h1').innerText;
+  const descricao = curso.querySelector('p').innerText;
+  const aulas = curso.querySelector('.aulas').innerText;
+  const horas = curso.querySelector('.horas').innerText;
   return {
-
+    titulo: titulo,
+    descricao,
+    aulas,
+    horas,
   }
 })
+
+console.log(objetoCursos);
+
+// Retorne uma lista com os
+// números maiores que 100
+
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+
+const maiorCem = numeros.filter(numero => numero > 100)
+console.log(maiorCem);
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
+
+const precoTotal = compras.reduce((contador, item)=>{
+    let numeroLimpo = +item.preco.replace('R$','').replace(',','.');
+    return contador += numeroLimpo;
+
+},0);
+
+console.log(precoTotal);
