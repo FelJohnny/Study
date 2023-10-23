fa0b35aa008a60f929408daede6e4c78bea39262
Felipe Johnny <felipijohnny@outlook.com>
2023-10-23 08:44:36 -0300

    estudando conceitos de  array com objetos

--- Front-End e Back-End/Origamid/JavaScript/15 - Array e Iterações/script.js
+++ Front-End e Back-End/Origamid/JavaScript/15 - Array e Iterações/script.js
@@ -42,6 +42,7 @@ const aulas = [
     min: 25
   },
 ]
+//-------------------------------------------------------------------------------------------//
 
 
 // Selecione cada curso e retorne uma array
@@ -67,15 +68,34 @@ const objetoCursos = arrayCursos.map((curso)=>{
 
 console.log(objetoCursos);
 
-// Retorne uma lista com os
-// números maiores que 100
+//-------------------------------------------------------------------------------------------//
+
+console.log("Verifique se Baixo faz parte da lista de instrumentos e retorne true");
+
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+console.log(instrumentos);
+
+const validaInstrumento = instrumentos.some((item)=> item === "Baixo");
+
+console.log(validaInstrumento);
+
+
+
+
+//-------------------------------------------------------------------------------------------//
+
+console.log("Retorne uma lista com os números maiores que 100");
 
 const numeros = [3, 44, 333, 23, 122, 322, 33];
 
 const maiorCem = numeros.filter(numero => numero > 100)
 console.log(maiorCem);
 
-// Retorne o valor total das compras
+
+
+//-------------------------------------------------------------------------------------------//
+console.log(" Retorne o valor total das compras")
+
 const compras = [
   {
     item: 'Banana',
@@ -98,7 +118,7 @@ const compras = [
     preco: 'R$ 10,60'
   }
 ]
-
+console.log(compras)
 const precoTotal = compras.reduce((contador, item)=>{
     let numeroLimpo = +item.preco.replace('R$','').replace(',','.');
     return contador += numeroLimpo;
