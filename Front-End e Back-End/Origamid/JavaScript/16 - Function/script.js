e4b7e821aa0c9b43269ebea5a603b932b7d71677
Felipe Johnny <felipijohnny@outlook.com>
2023-10-24 11:25:55 -0300

    estudando constructor de Funções

--- Front-End e Back-End/Origamid/JavaScript/16 - Function/script.js
+++ Front-End e Back-End/Origamid/JavaScript/16 - Function/script.js
@@ -60,9 +60,38 @@ function imc(altura, peso) {
   return peso / (altura * altura);
 }
 
-
+//Podemos passar argumentos padrões para uma função e retornar uma nova função.
 // esse imc180 esta utilizando o retorno de imc sem precisar declarar o 1.80 de altura pois nesse caso o medico por exemplo so atende pessoas de 1.80 metros
 const imc180 = imc.bind(null, 1.80);
 
 imc(1.80, 70); // 21.6
 imc180(70); // 21.6
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+//utilizando call e bind em um objeto
+
+const carro = {
+  marca: 'Ford',
+  ano: 2018,
+  acelerar: function(aceleracao, tempo) {
+    return `${this.marca} acelerou ${aceleracao} em ${tempo} segundos`;
+  }
+}
+carro.acelerar(100, 20);
+// Ford acelerou 100 em 20
+
+//criando outro objeto só possuindo marca
+const honda = {
+  marca: 'Honda',
+};
+
+
+//pegando metodo de carro para rodar em honda
+//a utilização do bind é usada justamente pra não executar no momento de declaração, somente quando utiizado o nome com: () pra executara funcção 
+const acelerarHonda = carro.acelerar.bind(honda);
+acelerarHonda(200, 10);
+// Honda acelerou 200 em 10
+
+
+//----------------------------------------------------------------------------------------------------------------------------//
