776997803a51e55c4a0158beef0742a81d1b592a
Felipe Johnny <felipijohnny@outlook.com>
2023-10-24 22:06:29 -0300

    aplicando estudos Objects constructors

--- Front-End e Back-End/Origamid/JavaScript/19 - Object Constructor 3/script.js
+++ Front-End e Back-End/Origamid/JavaScript/19 - Object Constructor 3/script.js
@@ -1,6 +1,3 @@
-console.log(Object.getOwnPropertyDescriptors(Array));
-console.log(Object.getOwnPropertyDescriptors(Array.prototype));
-
 const moto = {}
 
 Object.defineProperties(moto, {
@@ -22,29 +19,18 @@ Object.defineProperties(moto, {
   }
 });
 
-console.log(Object.getOwnPropertyDescriptors(moto));
-console.log('getOwnPropertyDescriptors pega uma propriedade ou metodo especifico do objeto moto')
-console.log(Object.getOwnPropertyDescriptor(moto, 'rodas'));
+console.log(".constructor mostra o tipo do contrudor do moto é um objeto")
+console.log(moto.constructor)
+console.log("hasOwnProperty, valida se a propriedade ou metodo existe no objeto")
+console.log(moto.hasOwnProperty('rodas'))
+console.log(moto.hasOwnProperty('map'))
+console.log("isPrototypeOf Valida se o construtor informado no inicio do argumento é prototype de moto, no caso nao é pois seria Object")
+console.log(Array.prototype.isPrototypeOf(moto));
+console.log(Object.prototype.isPrototypeOf(moto));
 
-console.log(Object.keys(moto)) // precisar ser enumerado
-
-console.log(Object.values(moto))
-
-console.log(Object.entries(moto))
-
-//lista os metodos e propriedades mesmo sendo não enumerados
-console.log("getOwnPropertyNames de Array")
-console.log(Object.getOwnPropertyNames(Array));
-console.log("getOwnPropertyNames de Array.proto")
-console.log(Object.getOwnPropertyNames(Array.prototype));
-
-console.log("getPrototypeOf Pega o prototype do construtor do objeto informado no caso de moto é object")
-console.log(Object.getPrototypeOf(moto));
-
-
-console.log("Object.is verifica se  o objeto é o mesmo e não se contem o mesmo conteudo")
-const frutas1 = ['Banana', 'Pêra'];
-const frutas2 = ['Banana', 'Pêra'];
-console.log(Object.is(frutas1, frutas2));
-
-//Object.freeze()impedir qualquer mudança nas propriedades. Object.seal()evite a adição de novas propriedades e impeça que as atuais sejam excluídas. Object.preventExtensions()prevenir a adição de novas propriedades.
+const li = document.querySelectorAll('li');
+console.log("meios de validar tipo do Objeto ")
+console.log("Com typeOf: ");
+console.log(typeof li); // object
+console.log("Com toString: ");
+console.log(li.toString());
