7b018a8f30fbb8fda1e258bc87d0f708e5ef76e8
Felipe Johnny <felipijohnny@outlook.com>
2023-11-15 15:57:58 -0300

    aplicando testes

--- React/Alura/useStates/use-states-pratica/src/App.js
+++ React/Alura/useStates/use-states-pratica/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  
+  const [endereco, setEndereco] = useState({})
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <input type="text" placeholder='DIGITE O CEP' onChange={evento => setEndereco({cep: evento.target.value})}/>
+        <li>CEP: {endereco.cep}</li>
+        
       </header>
     </div>
   );
