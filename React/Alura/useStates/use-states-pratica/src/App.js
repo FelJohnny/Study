20dadf58cc5aeffcb12cf66b8a789c8ad21cc34b
Felipe Johnny <felipijohnny@outlook.com>
2023-11-16 07:58:21 -0300

    finalizando estudo aplicando fetch em json de CEP com usestats

--- React/Alura/useStates/use-states-pratica/src/App.js
+++ React/Alura/useStates/use-states-pratica/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
 
@@ -6,12 +5,41 @@ function App() {
   
   const [endereco, setEndereco] = useState({})
 
+  function manipularEndereco(evento){
+    
+    const cep = evento.target.value
+    setEndereco({cep})
+
+    if(cep && cep.length === 8){
+      //obter cep
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(resposta => resposta.json())
+      .then(dados =>{
+        setEndereco(enderecoCepAntigo =>{
+          return{
+            ...enderecoCepAntigo,
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            uf: dados.uf
+          
+          }
+        })
+      })
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         
-        <input type="text" placeholder='DIGITE O CEP' onChange={evento => setEndereco({cep: evento.target.value})}/>
+        <input type="text" placeholder='DIGITE O CEP' onChange={manipularEndereco}/>
         <li>CEP: {endereco.cep}</li>
+        <li>Rua: {endereco.rua}</li>
+        <li>Bairro: {endereco.bairro}</li>
+        <li>Cidade: {endereco.cidade}</li>
+        <li>UF: {endereco.uf}</li>
         
       </header>
     </div>
