88e7c7166543f54d1920598d2e61b7c40fbe1ccf
Felipe Johnny <felipijohnny@outlook.com>
2023-11-21 16:59:39 -0300

    aplicando estudos em js assincrono promises then

---
+++ Front-End e Back-End/Origamid/JavaScript/30 - Js Assincrono/Js/index.js
@@ -0,0 +1,45 @@
+const promessa = new Promise((resolve, reject) =>{
+    let condicao =true;
+    if(condicao){
+        resolve({nome: 'johnny', idade:23})
+    }else{
+        reject(Error('Um erro ocorreu na promisse'))
+    }
+})
+
+
+promessa.then((resolucao) =>{ // primeiro argumento traz o resolve da promise
+    console.log(resolucao) //nome: 'johnny', idade:23
+});
+
+//alternativa mais curta de codigo
+
+promessa.then(retornoResolve => console.log(retornoResolve))
+
+//-------------------------------------------------------//
+
+const promessaAsync = new Promise((resolve, reject) =>{
+    let condicao =true;
+    if(condicao){
+        setTimeout(()=> {
+            resolve({nome: 'Joao', idade:20})
+        },2000);//2sec
+    }else{
+        reject(Error('Um erro ocorreu na promisse'))
+    }
+});
+
+
+const retorno = promessaAsync
+.then(retorno => {
+    console.log(retorno) //retorna o objeto promiseResult fica undefined por conta do objeto
+    retorno.profissao ='Ti'
+    return retorno
+})
+.then(retorno => 'teste')// sobrescreve apos execucao na promiseResult ficara 'teste'
+
+
+console.log(retorno) //antes dos 2secs a promisse 'promessaAsync' fica como pending
+
+//-------------------------------------------------------//
+
