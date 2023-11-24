de67ff2e9517c00fb1fa799cdbe50f612787db42
Felipe Johnny <felipijohnny@outlook.com>
2023-11-24 08:33:08 -0300

    praticando fetch em API's abertas

--- Front-End e Back-End/Origamid/JavaScript/31 - Js Assincrono Fetch/Js/index.js
+++ Front-End e Back-End/Origamid/JavaScript/31 - Js Assincrono Fetch/Js/index.js
@@ -64,5 +64,59 @@ const sobre = fetch('./index.html')
         corpo.appendChild(titulo) //adicionando o h1 e adicionando ao final do html
         console.log(div)
         console.log(titulo)
-    })
+})
 
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+// e o endereço completo é retornado ao clicar em buscar
+
+
+
+
+
+
+const cepInserido = document.getElementById('cepInput')
+const botao = document.getElementById('button')
+const retornoCep = document.querySelector('.cepExercicio')
+
+function buscaCEP(event){
+    event.preventDefault()
+    const cep = cepInserido.value
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.text()).then(body => {
+        retornoCep.innerText = body
+        
+    })
+}
+
+botao.addEventListener('click', buscaCEP)
+
+
+// Utilizando a API https://blockchain.info/ticker
+// retorne no DOM o valor de compra da bitcoin and reais.
+// atualize este valor a cada 30s
+
+
+const bitcoin = document.querySelector('.bitcoin')
+
+
+fetch("https://blockchain.info/ticker")
+.then(response => response.json())
+.then(body => bitcoin.innerText= ("R$ "+ body.BRL.buy).replace(".",","))
+
+
+// Utilizando a API https://api.chucknorris.io/jokes/random
+// retorne uma piada randomica do chucknorris, toda vez que
+// clicar em próxima
+
+const btnPiada = document.querySelector(".piada")
+const valuePiada = document.querySelector(".renderPiada")
+
+
+function buscaPiada(){
+    fetch("https://api.chucknorris.io/jokes/random")
+    .then(response => response.json())
+    .then(piada => valuePiada.innerText=piada.value)
+}
+
+btnPiada.addEventListener('click', buscaPiada)
