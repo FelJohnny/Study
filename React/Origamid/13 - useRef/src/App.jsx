5af7b85ac8d011352f1e8ce62364f1b8d70dfc44
Felipe Johnny <felipijohnny@outlook.com>
2023-11-30 07:53:13 -0300

    aplicando conceitos de useRef

--- React/Origamid/13 - useRef/src/App.jsx
+++ React/Origamid/13 - useRef/src/App.jsx
@@ -4,20 +4,36 @@ import React, { useEffect, useRef, useState } from "react";
 
 function App() {
 
-  const video = useRef();
   const [comentario, setComentario]= useState([]);
   const [inputText, setinputText]=useState('')
   const refInput = useRef();
-
+  //carrinho
+  const [carrinho, setCarrinho]=useState(0);
+  const [notificação, setNotificação]=useState(null)
+  const timeOut = useRef()
+  
   function handleclick(){
     setComentario([...comentario, inputText])
     setinputText('')
     refInput.current.focus();
   }
+
+
+
+  const video = useRef();
   useEffect(()=>{  
     console.log(video.current);
     },[video])
 
+
+    function adicionarCarrinho(){
+      setCarrinho (carrinho + 1)
+      setNotificação("Item adicionado no carrinho")
+      clearTimeout(timeOut.current)
+      timeOut.current = setTimeout(() => {
+        setNotificação(null)
+      }, 1000);
+    }
   return (
 
     <>
@@ -31,6 +47,10 @@ function App() {
         value={inputText}
         onChange={({target})=> setinputText(target.value)}/>
       <button onClick={handleclick}>click</button>
+
+      <h1>Carrinho:</h1>
+      <p>{notificação}</p>
+      <button onClick={adicionarCarrinho}>itens no carrinho: {carrinho}</button>
     </>
 
   );
