5630da238850ada2db636ba1384f8b2cd46a8c13
Felipe Johnny <felipijohnny@outlook.com>
2023-12-08 11:10:52 -0300

    aplicando custon hooks

---
+++ React/Origamid/16 - custon Hooks/hooks/useLocalStorage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useLocalStorage = (chave, inicial)=>{
+    const [estado, setEstado]=useState(() =>{
+        const local = window.localStorage.getItem(chave)
+        return local? local: inicial;
+    })
+
+    useEffect(()=>{
+        window.localStorage.setItem(chave, estado)
+    },[estado, chave])
+
+    return [estado, setEstado]
+}
+
+export default useLocalStorage
--- React/Origamid/16 - custon Hooks/src/App.jsx
+++ React/Origamid/16 - custon Hooks/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import useLocalStorage from '../hooks/useLocalStorage'
 
 
 function App() {
+  const [produto, setProduto] = useLocalStorage('produto','')
 
+  function handleclick({target}){
+    setProduto(target.innerText)
+  }
   return (
     <>
-      
+      <h2>Custom Hooks</h2>
+      <button onClick={handleclick}>book</button>
+      <button onClick={handleclick}>tablet</button>
     </>
   )
 }
