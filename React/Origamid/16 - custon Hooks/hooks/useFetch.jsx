baf64e57783ca1c19721d387a3da3175beda71fb
Felipe Johnny <felipijohnny@outlook.com>
2023-12-08 15:31:36 -0300

    aplicando custon hooks

---
+++ React/Origamid/16 - custon Hooks/hooks/useFetch.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+const useFetch = ()=>{
+    
+    const [data, setData]=useState(null)
+    const [error, setError]=useState(null)
+    const [loading, setLoading]=useState(null)
+
+    const request = async function(url,options){
+        
+    }
+}
