5feabc1c5217acb579f08fe2d6069a90d7b50443
Felipe Johnny <felipijohnny@outlook.com>
2024-01-24 10:41:10 -0300

    aplicando estilização com styled components

--- React/Praticas/cadastro e pesquisa/src/App.jsx
+++ React/Praticas/cadastro e pesquisa/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import './App.css'
 import Header from "./components/Header/Header"
 import Home from "./components/Home/Home"
 import Cadastrar from "./components/Cadastrar/Cadastrar"
--- React/Praticas/cadastro e pesquisa/src/components/Header/Header.jsx
+++ React/Praticas/cadastro e pesquisa/src/components/Header/Header.jsx
@@ -1,22 +1,60 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  display:flex;
+  align-items:center;
+  background-color: teal;
+  height:60px;
+
+`
+const Lista = styled.ul`
+  display:flex;
+  gap: 20px;
+  margin: 0 auto;
+  max-width:1000px;
+  justify-content:center;
+  list-style: none;
+
+`
+const ItemLista = styled.li`
+  transition: transform 0.3s;
+  &:hover {
+    background-color: #016969;
+    transform: scale(1.2);
+  }
+
+`
+
+const StyledNavLink = styled(NavLink)`
+  color: white;
+  padding: 10px 25px;
+  background-color: #004949;
+  border-radius:5px;
+  &:focus,
+  &:active {
+    outline: none;  
+    background-color: #2b9c9c;
+
+  } 
+`
+console.log(Nav)
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-            <li>
-                <NavLink to='/'>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to='/cadastrar'>Cadastrar</NavLink>
-            </li>
-            <li>
-                <NavLink to='/pesquisa'>Pesquisa</NavLink>
-            </li>
-        </ul>
-      </nav>
-    </div>
+      <Nav>
+        <Lista>
+            <ItemLista>
+                <StyledNavLink to='/'>Home</StyledNavLink>
+            </ItemLista>
+            <ItemLista>
+                <StyledNavLink to='/cadastrar'>Cadastrar</StyledNavLink>
+            </ItemLista>
+            <ItemLista>
+                <StyledNavLink to='/pesquisa'>Pesquisa</StyledNavLink>
+            </ItemLista>
+        </Lista>
+      </Nav>
   )
 }
 
