51ead1c04a046ed47e2e0c7af9b8c0708516715e
Felipe Johnny <felipijohnny@outlook.com>
2024-01-25 08:28:03 -0300

    aplicando styled components

--- React/Praticas/cadastro e pesquisa/src/components/Pesquisa/Pesquisa.jsx
+++ React/Praticas/cadastro e pesquisa/src/components/Pesquisa/Pesquisa.jsx
@@ -16,11 +16,13 @@ const InputPesquisa = styled.input`
   height:20px;
   padding: 5px 12px;
   font-size:1.2rem;
+  border:0;
   border-radius:10px;
 `
 const Table = styled.table`
   &>thead>tr>th{
-    background:red;
+    
+    text-align:start;
     padding-top:20px;
     width:1000px;
   }
@@ -30,7 +32,6 @@ const Pesquisa = () => {
 
   const {entidade} = useContext(GlobalContext)
   const [pesquisa, setPesquisa]=useState('')
-
 
   const filtrado = entidade.filter(pessoa =>{
     return pessoa.nome.includes(pesquisa)
@@ -49,16 +50,24 @@ const Pesquisa = () => {
           <thead>
             <tr>
               <th>Nome</th>
-              <th>Idade</th>
-              <th>Nota</th>
+              <th>Cpf</th>
+              <th>Telefone</th>
+              <th>Email</th>
+              <th>Desconto</th>
+              <th>Editar</th>
+              <th>Remover</th>   
             </tr>
           </thead>
       {filtrado.map(pessoa =>(
         <tbody key={pessoa.nome}>
           <tr>
             <td>{pessoa.nome}</td>
-            <td>{pessoa.idade}</td>
-            <td>{pessoa.nota1}</td>
+            <td>{pessoa.cpf}</td>
+            <td>{pessoa.telefone}</td>
+            <td>{pessoa.email}</td>
+            <td>{pessoa.desconto}</td>
+            <td><button>Editar</button></td>
+            <td><button>Remover</button></td>
           </tr>
         </tbody>
       ))}
