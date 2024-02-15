358daae64a1b67ba17c76811cf8d391b3f93eec7
Felipe Johnny <felipijohnny@outlook.com>
2024-02-15 08:14:59 -0300

    aplicando correção de request get

--- NodeJs/3 - nodejs-express-sequelize/src/controllers/PessoaController.js
+++ NodeJs/3 - nodejs-express-sequelize/src/controllers/PessoaController.js
@@ -1,12 +1,15 @@
-const database = require('../models')
+//const database = require('../models')
+const {Pessoa} = require('../models')
 class PessoaController{
 
     static async RetornaTodas(req,res){
         try {
-            const listaDePessoas = await database.findAll();
+            //const listaDePessoas = await database.Pessoa.findAll();
+            const listaDePessoas = await Pessoa.findAll();
             return res.status(200).json(listaDePessoas)
         } catch (error) {
-            console.log('deu erro')
+            console.log(`deu erro ${error}`)
+            return res.status(500).json(`deu erro, ${error}`)
         }
     }
 }
