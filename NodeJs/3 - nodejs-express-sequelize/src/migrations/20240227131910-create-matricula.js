9188d26a284ead01f498523a84e19962080c2196
Felipe Johnny <felipijohnny@outlook.com>
2024-02-27 12:11:20 -0300

    finalizando crfiação de tabelas e relacionamentos

--- NodeJs/3 - nodejs-express-sequelize/src/migrations/20240227131910-create-matricula.js
+++ NodeJs/3 - nodejs-express-sequelize/src/migrations/20240227131910-create-matricula.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Matriculas', {
+    await queryInterface.createTable('matriculas', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -33,6 +33,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Matriculas');
+    await queryInterface.dropTable('matriculas');
   }
 };
