6721a13f21a84142906eb0f7c58fa923a4179f96
Felipe Johnny <felipijohnny@outlook.com>
2024-03-13 20:15:45 -0300

    aplicando exercicios em typeScript

---
+++ TypeScript/02 - exercicio1/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const texto = "    texto qualquer ";
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
+}
+console.log(texto);
+console.log(normalizarTexto(texto));
---
+++ TypeScript/02 - exercicio1/script.ts
@@ -0,0 +1,9 @@
+const texto = "    texto qualquer "
+
+function normalizarTexto(texto: string) {
+   return texto.trim().toLowerCase();
+   
+}
+console.log(texto);
+console.log(normalizarTexto(texto));
+ 
