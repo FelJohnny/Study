a1bc13f6237f5372080ea1c974e348abadf913ca
Felipe Johnny <felipijohnny@outlook.com>
2024-07-04 13:34:51 -0300

    aplicando e entendendo conceitos de Link em nextJs

---
+++ Next.js/03-link/src/app/sobre/page.tsx
@@ -0,0 +1,8 @@
+export default function SobrePage(){
+    return(
+        <>
+            <h1>Sobre</h1>
+            <h2 style={{margin:'1600px 0'}} id="empresa">A empresa</h2>
+        </>
+    )
+}
---
+++ Next.js/03-link/src/components/Menu/menu.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Menu(){
+    return(
+        <>
+            <ul className="menu">
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/contato'}>Contato</Link></li>
+                <li><Link href={'/sobre#empresa'}>Sobre</Link></li>
+            </ul>
+        </>
+    )
+}
